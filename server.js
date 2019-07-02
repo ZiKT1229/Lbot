c7f7b288af170273cf0d95f18f52b59a2c75f997
KT_Zheng <a6163508@gmail.com>
2019-07-02 14:13:48 +0800

    line bot with weather

---
+++ server.js
@@ -0,0 +1,51 @@
+const fetch = require('node-fetch');
+const linebot = require('linebot');
+const bot = linebot({
+  channelId: process.env.channelId,
+  channelSecret: process.env.channelSecret,
+  channelAccessToken: process.env.channelAccessToken
+});
+const url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001';
+const Authorization = 'CWB-A3345325-4A6E-4279-80B2-9491AF9A61B1';
+const elementName = 'Wx';
+const weather = [];
+const getData = async() => {
+  const endpoint = `${url}?Authorization=${Authorization}&elementName=${elementName}`;
+  try {
+    const response = await fetch(endpoint);
+    if (response.ok) {
+      const { records } = await response.json();
+      const { location } = records;
+      location.forEach(_location => {
+        weather[_location.locationName] = _location.weatherElement[0].time[0].parameter.parameterName;
+      });
+    } else {
+      throw new Error('Response is not ok!');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+bot.on('message', function (event) {
+  getData();
+  const msg = event.message.text;
+  let replyMsg = '';
+
+  if (weather[msg]) {
+    replyMsg = `${msg}:${weather[msg]}`;
+  } else {
+    replyMsg = '請輸入正確縣市名稱，注意: 臺(O) 台(X)';
+  }
+
+  event.reply(replyMsg).then(function (data) {
+    
+  }).catch(function (error) {
+    console.log(error);
+  });
+});
+
+const port = process.env.PORT || 3000;
+bot.listen('/linewebhook', port, function () {
+  console.log('[BOT已準備就緒]');
+});
